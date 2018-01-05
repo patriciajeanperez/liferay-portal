bee4c341db85dd978b076d2930871fbdd0f6a1b7
patriciajeanperez <patricia.perez@liferay.com>
2018-01-05 15:37:29 -0800

    LPS-75319 Move implementation into module (reverted from commit 12838afa6a55227eb37231c8515833fc60ecd517)

--- portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadFlagLocalServiceImpl.java
+++ portal-impl/src/com/liferay/portlet/messageboards/service/impl/MBThreadFlagLocalServiceImpl.java
@@ -18,10 +18,12 @@ import com.liferay.message.boards.kernel.model.MBThread;
 import com.liferay.message.boards.kernel.model.MBThreadFlag;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.systemevent.SystemEvent;
 import com.liferay.portal.kernel.transaction.Propagation;
 import com.liferay.portal.kernel.transaction.Transactional;
+import com.liferay.portal.kernel.util.DateUtil;
 import com.liferay.portlet.messageboards.service.base.MBThreadFlagLocalServiceBaseImpl;
 
 import java.util.List;
@@ -29,10 +31,7 @@ import java.util.List;
 /**
  * @author Brian Wing Shun Chan
  * @author Shuyang Zhou
- * @deprecated As of 7.0.0, replaced by {@link
- *             com.liferay.message.boards.service.impl.MBThreadFlagLocalServiceImpl}
  */
-@Deprecated
 public class MBThreadFlagLocalServiceImpl
 	extends MBThreadFlagLocalServiceBaseImpl {
 
@@ -42,10 +41,43 @@ public class MBThreadFlagLocalServiceImpl
 			long userId, MBThread thread, ServiceContext serviceContext)
 		throws PortalException {
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		if (user.isDefaultUser()) {
+			return null;
+		}
+
+		long threadId = thread.getThreadId();
+
+		MBThreadFlag threadFlag = mbThreadFlagPersistence.fetchByU_T(
+			userId, threadId);
+
+		if ((threadFlag != null) &&
+			DateUtil.equals(
+				threadFlag.getModifiedDate(), thread.getLastPostDate())) {
+
+			return threadFlag;
+		}
+
+		if (threadFlag == null) {
+			long threadFlagId = counterLocalService.increment();
+
+			threadFlag = mbThreadFlagPersistence.create(threadFlagId);
+
+			threadFlag.setUuid(serviceContext.getUuid());
+			threadFlag.setGroupId(thread.getGroupId());
+			threadFlag.setCompanyId(user.getCompanyId());
+			threadFlag.setUserId(userId);
+			threadFlag.setUserName(user.getFullName());
+			threadFlag.setModifiedDate(
+				serviceContext.getModifiedDate(thread.getLastPostDate()));
+			threadFlag.setThreadId(threadId);
+		}
+		else {
+			threadFlag.setModifiedDate(thread.getLastPostDate());
+		}
+
+		return mbThreadFlagLocalService.updateMBThreadFlag(threadFlag);
 	}
 
 	@Override
@@ -53,19 +85,13 @@ public class MBThreadFlagLocalServiceImpl
 		MBThreadFlag threadFlag = mbThreadFlagPersistence.findByPrimaryKey(
 			threadFlagId);
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		mbThreadFlagLocalService.deleteThreadFlag(threadFlag);
 	}
 
 	@Override
 	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
 	public void deleteThreadFlag(MBThreadFlag threadFlag) {
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		mbThreadFlagPersistence.remove(threadFlag);
 	}
 
 	@Override
@@ -73,10 +99,9 @@ public class MBThreadFlagLocalServiceImpl
 		List<MBThreadFlag> threadFlags = mbThreadFlagPersistence.findByThreadId(
 			threadId);
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		for (MBThreadFlag threadFlag : threadFlags) {
+			mbThreadFlagLocalService.deleteThreadFlag(threadFlag);
+		}
 	}
 
 	@Override
@@ -84,30 +109,46 @@ public class MBThreadFlagLocalServiceImpl
 		List<MBThreadFlag> threadFlags = mbThreadFlagPersistence.findByUserId(
 			userId);
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		for (MBThreadFlag threadFlag : threadFlags) {
+			mbThreadFlagLocalService.deleteThreadFlag(threadFlag);
+		}
 	}
 
 	@Override
 	public MBThreadFlag getThreadFlag(long userId, MBThread thread)
 		throws PortalException {
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		if (user.isDefaultUser()) {
+			return null;
+		}
+
+		return mbThreadFlagPersistence.fetchByU_T(userId, thread.getThreadId());
 	}
 
 	@Override
 	public boolean hasThreadFlag(long userId, MBThread thread)
 		throws PortalException {
 
-		throw new UnsupportedOperationException(
-			"This class is deprecated and replaced by " +
-				"com.liferay.message.boards.service.impl." +
-					"MBThreadFlagLocalServiceImpl");
+		User user = userPersistence.findByPrimaryKey(userId);
+
+		if (user.isDefaultUser()) {
+			return true;
+		}
+
+		MBThreadFlag threadFlag = mbThreadFlagPersistence.fetchByU_T(
+			userId, thread.getThreadId());
+
+		if ((threadFlag != null) &&
+			DateUtil.equals(
+				threadFlag.getModifiedDate(), thread.getLastPostDate())) {
+
+			return true;
+		}
+		else {
+			return false;
+		}
 	}
 
 }
