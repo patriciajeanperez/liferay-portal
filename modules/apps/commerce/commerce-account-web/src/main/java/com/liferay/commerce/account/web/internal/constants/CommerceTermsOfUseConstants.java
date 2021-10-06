2eabc956d79e77eacf846bbde71318d07da61e0f
drewbrokke <drew.brokke@liferay.com>
2021-10-05 19:11:13 -0500

    LPS-139772 commerce-account-web: adds constants file for terms of use URLs

---
+++ modules/apps/commerce/commerce-account-web/src/main/java/com/liferay/commerce/account/web/internal/constants/CommerceTermsOfUseConstants.java
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.commerce.account.web.internal.constants;
+
+/**
+ * @author Drew Brokke
+ */
+public class CommerceTermsOfUseConstants {
+
+	public static final String COMMERCE_ACTIVATION_DOCUMENTATION_URL =
+		"https://learn.liferay.com/commerce/latest/en/installation-and-" +
+			"upgrades/activating-liferay-commerce-enterprise.html";
+
+	public static final String LIFERAY_SALES_EMAIL_ADDRESS =
+		"sales@liferay.com";
+
+}
